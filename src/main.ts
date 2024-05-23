7f2826b90609cac524d949c05d5661ca91cd3c3b
Rajesh Saini <rajeshsaini1490@gmail.com>
2024-05-23 18:17:38 +0530

    changes to support vercel serverless

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  await app.init();
 }
+
 bootstrap();
+
+export const handler = serverless(expressApp);
